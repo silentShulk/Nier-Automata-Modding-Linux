f1e59f852c4c4e39c151fe7873b2c4871215b6cd
silentShulk <marcostrada258@gmail.com>
2026-01-17 17:25:40 +0100

    Tried to start working on installation methods, need to understand how to get game_path from there

--- NAMHL/src/mod_managing/installation_methods.rs
+++ NAMHL/src/mod_managing/installation_methods.rs
@@ -1,8 +1,15 @@
-//use std
+use std::fs::read_dir;
 use std::path::PathBuf;
 
 pub fn install_texture(dss_folder_path: PathBuf) {
-
+    let dss_files = read_dir(dss_folder_path).unwrap();
+    let textures_folder = PathBuf::from("");
+    
+    for file in dss_files {
+        let entry = file.expect("Couldn't read one of the textures files");
+        let test_path = entry.path();
+        
+    }
 }
 
 pub fn install_player_model(dtt_dat_folder_path: PathBuf) {
